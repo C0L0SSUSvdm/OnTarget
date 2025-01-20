8f30a79d21f6afe71723f16d0bedc42dcc9a18ac
Tyler Reber <94849230+TAReber@users.noreply.github.com>
2025-01-20 17:12:59 -0500

    Added Matt's Spedometer UI
    Spedometer UI Updated
    Level Image Update
    Solution added to keep car from getting stuck on head when rolls over

--- Racer/Assets/Scripts/UI/HUD/HUD_Speedometer_Default.cs
+++ Racer/Assets/Scripts/UI/HUD/HUD_Speedometer_Default.cs
@@ -1,3 +1,4 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,10 +6,22 @@ using UnityEngine;
 
 public class HUD_Speedometer_Default : MonoBehaviour, HUD_Speedometer_Director
 {
+    [SerializeField] GameObject needle;
     [SerializeField] public TextMeshProUGUI speedometerText;
-
+    
+    //12 degree is zero, subtract 12
     public void UpdateSpeedometer(float speed)
     {
-        speedometerText.text = (speed * 0.625f).ToString("F0");
+        //164 from 0 to max
+        float convertedspeed = (speed * 0.625f);
+        float spedometerangle = (speed / 160.0f) * 164;
+
+
+        Quaternion roation = Quaternion.Euler(0, 0, -(spedometerangle - 10.5f));
+        needle.transform.rotation = roation;
+        
+
+
+        speedometerText.text = speed.ToString("F0");
     }
 }
